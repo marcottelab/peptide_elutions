85d44fe33cb195f39804833eda4bfdca05a98ea9
Claire McWhite <cmcwhite@marccomp01.ccbb.utexas.edu>
2022-09-05 10:28:10 -0500

    Fix terminal bias.R

--- scripts/terminal_bias.R
+++ scripts/terminal_bias.R
@@ -3,6 +3,7 @@ library(argparse)
 library(furrr)
 library(purrr)
 library(future)
+library(argparse)
 plan("multisession")
 #This script is used to calculate terminal bias score for each protein in a fractionation experiment. Only works with R>4.0.
 
@@ -20,55 +21,79 @@ parser$add_argument('--output_file', dest='output_file', action='store', require
 
 args = parser$parse_args()
 
-print("load functions for terminal bias calculation")
-
-library(tidyverse)
-#library(argparse)
-
-elut_wide_file <- '/Users/mwsaelee/Desktop/peptide_elutions-master/test/pivot_test.csv'
-peps_file <- '/Users/mwsaelee/Desktop/peptide_elutions-master/test/uniprot_human_digested.csv'
-fraction_order_file  <- '/Users/mwsaelee/Desktop/peptide_elutions-master/test/fraction_order_test.csv'
+get_terminal_ranges <- function(oneexp){
+  firstobs <- oneexp %>%
+    filter(presence == "observed") %>%
+    filter(Start == min(Start)) %>% pull(Start) %>% head(1)
+  lastobs <- oneexp %>%
+    filter(presence == "observed") %>%
+    filter(Start == max(Start)) %>% pull(Start) %>% head(1)
   
+  # observed = in this experiment, possible = seen at all across experiments / detectable
+  # Look from first possible peptide to last observed peptide
+  missing_n <- oneexp %>%
+    filter(Start <= lastobs)
+  # Look from first observed peptide to last possible peptide
+  missing_c <- oneexp %>%
+    filter(Start >= firstobs)
+  return(list(missing_n$Start, missing_c$Start))
+}
 
-elut_wide <- read_delim(elut_wide_file,  delim = ",") 
-fraction_order <- read_delim(fraction_order_file,  delim = ",")
-peps <- read_delim(peps_file,  delim = ",")
-
-print("Identify peptides")
-elut_wide_identified <- elut_wide %>%
-  inner_join(peps, by = "Peptide")
-
-print("Identify proteins with no peptides")
-proteins_w_unique_peps <- elut_wide_identified %>%
-  select(ProteinID, ExperimentID) %>%
-  unique
-
-print("Convert wide to tidy")
-elut_long <- elut_wide_identified %>%
-  pivot_longer(cols = starts_with("MB_Sup_HSA"),
-               names_to = "FractionID",
-               values_to = "pepcount") %>%
-  mutate(pepcount = as.numeric(pepcount)) %>%
-  left_join(fraction_order, by = "FractionID") %>%
-  mutate(FractionID = str_replace(FractionID, "^fractionid_", "")) %>%
-  filter(!is.na(pepcount)) # Unneeded
-
-#fraction_order %>% write_csv(paste0(elut_wide_file, "fraction_order.csv"))
-
-elut_long %>% write_csv(paste0(elut_wide_file, ".annot.long.tidy"))
-
-total_fractions <- c(nrow(fraction_order))
+get_missing_obs_counts <- function(start, oneexp){
+  
+  df <-oneexp %>%
+    group_by(ProteinID, peak, experiment_name) %>%
+    mutate(fragment_end = case_when(Start < {{ start }}  ~ "nterm",
+                                    Start >= {{ start }} ~ "cterm")
+    ) %>% ungroup %>%
+    group_by(ProteinID, peak, experiment_name, fragment_end) %>%
+    dplyr::count(presence) %>% # Overwritted by seqinr
+    ungroup %>%
+    mutate(n= n + 1) %>%
+    pivot_wider(names_from = c(presence, fragment_end), values_from = n, values_fill = 1) %>%
+    mutate(bp_start = {{ start }})
+  return(df)
+}
 
 
-#10x smaller
-elut_short <- elut_long %>% 
-  filter(pepcount > 0) %>%
-  mutate(totfracts = total_fractions)
+get_bps <- function(oneexp){
+  # Terminal bias calculation
+  missing <- get_terminal_ranges(oneexp)
+  missing_n <- missing[[1]]
+  missing_c <- missing[[2]]
+  
+  print(missing_n)
+  # C terminal fragments are missing n termini
+  c_fragments <- map_dfr(missing_n, ~get_missing_obs_counts(., oneexp)) 
+  
+  c_fragments$terminal <- "cterm_extension"
+  # N terminal fragments are missing c termini
+  n_fragments <- map_dfr(missing_c, ~get_missing_obs_counts(., oneexp)) 
+    
+   n_fragments$terminal <-"nterm_extension"
+  
+  
+  fragments <- bind_rows(n_fragments, c_fragments) %>%
+    mutate_at(vars(matches("_[nc]term")), replace_na, 1) 
+  
+  fragments <- fragments %>%
+    
+    mutate(       ratio_cterm = observed_cterm/(observed_cterm + missing_cterm),
+                  ratio_nterm = observed_nterm/(observed_nterm + missing_nterm)
+    ) %>% 
+    #select(-breakpointleft_pepid, -breakpointright_pepid) %>%
+    mutate(fc = ratio_nterm/ratio_cterm) %>%
+    mutate(log2fc = log2(fc)) %>%
+    mutate(abs_log2fc = abs(log2fc))
+  
+  return(fragments)
+  
+}
 
 
+possibly_get_bps <- possibly(get_bps, otherwise = data.frame())
 
 
-elut_short %>% write_csv(paste0(elut_wide_file, ".annot.short.tidy"))
 
 
 
@@ -120,6 +145,7 @@ short_tidy_unique_peaks_expanded_fragments  <- short_tidy_unique_peaks_expanded 
   semi_join(enough_cov_peaks, by = c("ProteinID", "peak", "experiment_name"))
 
 bps <- short_tidy_unique_peaks_expanded_fragments %>%
+  filter(grepl("UBA6_H", ProteinID)) %>%
   arrange(Start, End)%>%
   split(list(.$ProteinID, .$experiment_name, .$peak)) %>%
   future_map_dfr(~possibly_get_bps(.))
